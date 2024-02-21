c0a4e8858c887ba8f0bead7cba256649b75cc154
muharremtekin <muharremtekinn@outlook.com.tr>
2024-02-21 14:40:01 +0300

    Add project files.

---
+++ DrawingProject.Client/Hubs/DrawHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DrawingProject.Client.Hubs;
+
+public class DrawHub : Hub
+{
+    public async Task SendMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId}: {message}");
+    }
+
+    public async Task NewStroke(Point start, Point end)
+    {
+        await Clients.Others.SendAsync("newStroke", start, end);
+    }
+}
+
---
+++ DrawingProject.Client/Hubs/Point.cs
@@ -0,0 +1,8 @@
+﻿namespace DrawingProject.Client.Hubs;
+
+public record Point
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+}
+
